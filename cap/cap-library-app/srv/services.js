c4c1b630612931f06c57b65730b2a8c604729168
kastrubait <mitaka@tut.by>
2023-02-12 14:16:36 +0000

    feat: auto calculation IDs for entities

--- cap/cap-library-app/srv/services.js
+++ cap/cap-library-app/srv/services.js
@@ -2,7 +2,27 @@ const cds = require('@sap/cds');
 
 module.exports = cds.service.impl(async function () {
 
-    let { Booking } = this.entities;
+    let { Books, Readers, Authors, Booking } = this.entities;
+
+    this.before('NEW', 'Books', async (req) => {
+        const { maxID } = await SELECT.one`max(bookID) as maxID`.from(Books);
+        req.data.bookID = maxID + 1;
+    });
+
+    this.before('NEW', 'Readers', async (req) => {
+        const { maxID } = await SELECT.one`max(readerID) as maxID`.from(Readers);
+        req.data.readerID = maxID + 1;
+    });
+
+    this.before('NEW', 'Authors', async (req) => {
+        const { maxID } = await SELECT.one`max(authorID) as maxID`.from(Authors);
+        req.data.authorID = maxID + 1;
+    });
+
+    this.before('NEW', 'Booking', async (req) => {
+        const { maxID } = await SELECT.one`max(bookingID) as maxID`.from(Booking);
+        req.data.bookingID = maxID + 1;
+    });
 
     this.on('returnTheBook', 'Booking', async (req) => {
         const { bookingUUID } = req.params[0];
