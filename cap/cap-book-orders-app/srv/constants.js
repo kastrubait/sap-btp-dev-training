1ca5d2fc55f78d26aeeea5aecfc8f97e1e549dd7
kastrubait <mitaka@tut.by>
2023-02-20 10:11:45 +0000

    feat: add custom logic

---
+++ cap/cap-book-orders-app/srv/constants.js
@@ -0,0 +1,8 @@
+let genericErrors;
+module.exports = Object.freeze({
+    genericErrors : {
+        processingOrder : 'Order in the appoval process. ',
+        orderAppruved    : 'The order has alredy been appruved.',
+        booksNotAvialable   : 'Books are not avialable for delivery.'
+    } 
+})
